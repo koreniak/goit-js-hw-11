37cc5952f21eddfdb2a3b133f53d57c9b3b20499
Borys Koreniak <koreniak.borys@gmail.com>
2023-04-28 17:19:02 +0300

    Вніс початкові дані, підключив бібліотеки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './css/styles.css';
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const API_KEY = '35861532-f4480698535ddfd2cf92b517e';
+const BASE_URL = 'https://pixabay.com/api/';
